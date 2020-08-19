802c3912c8e32177cc13a55d819e2b30c2cb9f29
Thanasi Bakis <thanasibakis@gmail.com>
2020-08-19 00:23:16 -0700

    Bug fixes but still non-functional
    stream.readlines() taking too long
    
    2020-08-19 00:22:00.822514 	 readlines()
    2020-08-19 00:22:02.581443 	 queue.put()
    2020-08-19 00:22:02.581443 	 readlines()
    2020-08-19 00:22:04.453220 	 queue.put()
    2020-08-19 00:22:04.453220 	 readlines()
    2020-08-19 00:22:06.292825 	 queue.put()
    2020-08-19 00:22:06.297611 	 readlines()
    2020-08-19 00:22:07.912845 	 queue.put()
    2020-08-19 00:22:07.912845 	 readlines()

--- src/Sensor.py
+++ src/Sensor.py
@@ -19,10 +19,8 @@ class Sensor:
 
         # The method that actually reads from the device needs to be in its own thread.
         # This way, if there's nothing coming from it, we aren't holding up the GUI thread.
-        self._data_fetching_thread = Thread(target=_data_fetching_loop, args=(stream, batch_size, self.data_queue))
+        self._data_fetching_thread = Thread(target=_data_fetching_loop, args=(self.stream, self.data_queue, self.batch_size))
         self._data_fetching_thread.start()
-
-        log_message(0, repr(self._data_fetching_thread))
 
     # Basically the core of the research problem-- turn raw acceleration into meaningful position
     def accumulate_next_batch(self):
@@ -30,11 +28,26 @@ class Sensor:
             # Collect new raw acceleration data, if available.
             # Otherwise, throws Empty exception
             log_message(2, "Fetching data")
-            new_samples = self.data_queue.get_nowait()
+            new_bytes = self.data_queue.get_nowait()
 
             # Don't process/save this data if we don't want it
             if(not self.should_record):
                 return
+
+            # Parse the bytes into a pandas data frame
+            new_samples = []
+
+            for line in new_bytes:
+                try:
+                    line = line.decode("utf-8").strip()
+
+                    if line.startswith("mugicdata"): # Keep only lines with mugicdata prefix
+                        new_samples.append(line.split(' ')[1:])  # but lose that prefix
+                
+                except:
+                    log_message(1, "A line of data was corrupt. This is likely because you are running on serial mode and read the data mid-line. This line will be thrown out and is probably nothing to worry about.")
+
+            new_samples = pd.DataFrame(new_samples, columns = config.COLUMNS, dtype = "double").set_index("time_sec")
 
             # Don't calculate position if we haven't calibrated enough
             # ("Calibrating" == accumulating enough data to fulfill reuse size requirement)
@@ -75,20 +88,20 @@ class Sensor:
 
                 # Retrieve average velocity of previous batch
                 # (or specifically, the portion of that batch that overlaps with this one)
-                #mean_old_v = self.accumulated_data[["vx", "vy", "vz"]] \
-                #    .tail(self.reuse_size - self.batch_size) \
-                #    .mean(axis = 0)
+                mean_old_v = self.accumulated_data[["vx", "vy", "vz"]] \
+                    .tail(self.reuse_size - self.batch_size) \
+                    .mean(axis = 0)
 
                 # Retrieve average velocity of new batch
                 # (again, specifically the portion that overlaps)
-                #mean_new_v = new_velocity \
-                #    .head(self.reuse_size - self.batch_size) \
-                #    .mean(axis = 0)
+                mean_new_v = new_velocity \
+                    .head(self.reuse_size - self.batch_size) \
+                    .mean(axis = 0)
 
                 # Add a constant to velocity such that mean_old_v == mean_new_v
-                #offset = mean_old_v - mean_new_v
-                #new_velocity = new_velocity.apply(lambda row: row + offset,
-                #                                  axis = 1)
+                offset = mean_old_v - mean_new_v
+                new_velocity = new_velocity.apply(lambda row: row + offset,
+                                                  axis = 1)
 
                 ## End integration correction ##
 
@@ -105,20 +118,20 @@ class Sensor:
 
                 # Retrieve average position of previous batch
                 # (or specifically, the portion of that batch that overlaps with this one)
-                #mean_old_p = self.accumulated_data[["x", "y", "z"]] \
-                #    .tail(self.reuse_size - self.batch_size) \
-                #    .mean(axis = 0)
+                mean_old_p = self.accumulated_data[["x", "y", "z"]] \
+                    .tail(self.reuse_size - self.batch_size) \
+                    .mean(axis = 0)
 
                 # Retrieve average position of new batch
                 # (again, specifically the portion that overlaps)
-                #mean_new_p = new_position \
-                #    .head(self.reuse_size - self.batch_size) \
-                #    .mean(axis = 0)
+                mean_new_p = new_position \
+                    .head(self.reuse_size - self.batch_size) \
+                    .mean(axis = 0)
 
                 # Add a constant to position such that mean_old_p == mean_new_p
-                #offset = mean_old_p - mean_new_p
-                #new_position = new_position.apply(lambda row: row + offset,
-                #                                  axis = 1)
+                offset = mean_old_p - mean_new_p
+                new_position = new_position.apply(lambda row: row + offset,
+                                                  axis = 1)
 
                 ## End integration correction ##
 
@@ -142,7 +155,7 @@ class Sensor:
             
         except Empty:
             # The data fetching thread had nothing, so don't hold up the GUI thread... just return out
-            return
+            log_message(2, "No data was queued.")
     
         except Exception as ex:
             log_message(1, repr(ex))
@@ -175,26 +188,15 @@ def log_message(error_level, msg):
     if(error_level <= config.DEBUG_LEVEL):
         print(datetime.now(), '\t', msg)
 
-# Converts and accumulates the raw output from the sensor to a Pandas data frame
+# Constantly pushes data from the sensor into a queue for processing on demand
 def _data_fetching_loop(stream, data_queue, batch_size):
     try:
         while True:
+            # TODO: why is reading taking longer now vs the previous codebase version?
+            log_message(1, "readlines()") # temporary
             raw_bytes = stream.readlines(batch_size)
-
-            samples = []
-
-            for line in raw_bytes:
-                try:
-                    line = line.decode("utf-8").strip()
-
-                    if line.startswith("mugicdata"): # Keep only lines with mugicdata prefix
-                        samples.append(line.split(' ')[1:])  # but lose that prefix
-                
-                except:
-                    log_message(1, "A line of data was corrupt. This is likely because you are running on serial mode and read the data mid-line. This line will be thrown out and is probably nothing to worry about.")
-
-            samples = pd.DataFrame(samples, columns = config.COLUMNS, dtype = "double").set_index("time_sec")
-            data_queue.put(samples)
+            log_message(1, "queue.put()") # temporary
+            data_queue.put(raw_bytes)
 
     except:  # the connection was closed, so this thread needs to end
-        log_message(2, "Fetching has been halted.")
+            log_message(1, "Fetching has been halted.")
